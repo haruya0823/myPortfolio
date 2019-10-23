730595fea65bf584157f327f3d2a018d459f1a5e
haruya0823 <ssharuya0823@gmail.com>
2019-10-24 04:22:57 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const welcome = window.confirm('are you sure?');
+// if(welcome === true) {
+//   document.write("大成功！");
+// };
+// if(welcome === false) {
+//   document.write("入力を確認してください。");
+// };
+
+'use strict';
